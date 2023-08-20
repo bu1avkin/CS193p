bdce61a9854ef2be0436a8b5742ea74ed4f737f8
Aleksey Bulatov <alekseybulatov.study@gmail.com>
2023-08-20 16:15:23 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Леша Булатов on 20.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
